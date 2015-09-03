038889b8e3d77923fb7bd578f3527f30e3189362
Andy <lunchboxdc@gmail.com>
2015-09-03 18:57:07 -0500

    adding code to serve out html files

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
 app.get('/', function (req, res) {
-	  res.send('Hello World!');
+	  res.sendFile(path.join(__dirname+'/templates/helloWorld.html'));
 });
 
 var server = app.listen(3000, function () {
