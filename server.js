a11997413a88193ab0bf4810b97021523776cfa3
Joseph Jung <joejung@Administrators-MacBook-Pro-2.local>
2015-09-03 19:28:24 -0500

    Add react and babel and a component

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 var express = require('express');
+require("babel/register");
+var React = require('react');
 var path = require('path');
 var app = express();
 var routes = require('./routes.json');
 
 app.get('/*', function (req, res) {
 
-    var url = req.originalUrl;
-    var template = routes.route.filter(function(route){
-    	return route.path === url;
-    })[0].template;
+    //var url = req.originalUrl;
+    //var template = routes.route.filter(function(route){
+    //	return route.path === url;
+    //})[0].template;
 
-    res.sendFile(path.join(__dirname + "/templates/" + template));
+    var HelloComponent = require('./component/hello-component.jsx');
+    var HelloComponentInstance = React.createElement(HelloComponent, { name: "World" });
+
+    res.send(React.renderToString(HelloComponentInstance));
 
 });
 
