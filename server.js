efea0c668e3f71281ae898baf08ffb791434ac7d
Joseph Jung <joejung@Administrators-MacBook-Pro-2.local>
2015-09-03 19:01:00 -0500

    hook up a few things.

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var routes = require('./routes.json');
 
-app.get('/', function (req, res) {
-	  res.sendFile(path.join(__dirname+'/templates/helloWorld.html'));
+app.get('/*', function (req, res) {
+
+    var url = req.originalUrl;
+    var template = routes[url];
+
+    res.sendFile(path.join(__dirname + "/templates/" + template));
+
 });
 
 var server = app.listen(3000, function () {
-	  var host = server.address().address;
-	    var port = server.address().port;
 
-		  console.log('Example app listening at http://%s:%s', host, port);
+    var host = server.address().address;
+    var port = server.address().port;
+
+      console.log('Example app listening at http://%s:%s', host, port);
 });
